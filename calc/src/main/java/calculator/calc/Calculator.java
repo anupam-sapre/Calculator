3e514263e5ebad7eccc2d6d0acd434aa0eb29bc6
anupamsapre <anupamsapre@ccs.neu.edu>
2017-08-19 21:01:50 -0400

    Changes for dependency jar creation
    pom.xml configuration changed

--- calc/src/main/java/calculator/calc/Calculator.java
+++ calc/src/main/java/calculator/calc/Calculator.java
@@ -17,35 +17,54 @@ public class Calculator {
 				int val = calculate(input,new HashMap<String,Integer>());
 				log.info("Result is "+val);
 				System.out.println(val);
+			}catch(NumberFormatException nr){
+				System.out.println("Please enter valid input format");
+				//nr.printStackTrace();
 			}catch(IllegalArgumentException e){
-				e.printStackTrace();
+				System.out.println("Please enter valid input format");
+				//e.printStackTrace();
 			}catch(ArithmeticException ar){
-				ar.printStackTrace();
-				
+				System.out.println("Divide by zero is not feasible");
+				//ar.printStackTrace();		
+			}catch(RuntimeException re){
+				System.out.println("Please enter input with result in Integer range");
+				//re.printStackTrace();
 			}
 		}else{
 			System.out.println("Please enter a input for program.");
 		}
-
 	}
 
 
-	public static int calculate(String input,HashMap<String,Integer> variables)  throws IllegalArgumentException,
-	ArithmeticException{
+	/**
+	 * @param input
+	 * @param variables
+	 * @return
+	 * @throws IllegalArgumentException
+	 * @throws ArithmeticException
+	 * Method to analyze operator in expression and calculate result
+	 */
+	public static int calculate(String input,HashMap<String,Integer> variables)  
+			throws IllegalArgumentException,ArithmeticException,RuntimeException,NumberFormatException{
 		int result =0;
-
-		if(input.substring(0,3).equals(Constants.add)){
-			result = processAdd(input.substring(4,input.length()-1),variables);
-		}else if(input.substring(0,3).equals(Constants.let)){
-			result = processLet(input.substring(4,input.length()-1),variables);
-		}else if(input.substring(0,3).equals(Constants.sub)){
-			result = processSub(input.substring(4,input.length()-1),variables);
-		}else if(input.substring(0,3).equals(Constants.div)){
-			result = processDiv(input.substring(4,input.length()-1),variables);
-		}else if(input.substring(0,4).equals(Constants.mult)){
-			result = processMultiply(input.substring(5,input.length()-1),variables);
+		if(input.length() > 5){
+			if(input.substring(0,3).equals(Constants.add)){
+				result = processAdd(input.substring(4,input.length()-1),variables);
+			}else if(input.substring(0,3).equals(Constants.let)){
+				result = processLet(input.substring(4,input.length()-1),variables);
+			}else if(input.substring(0,3).equals(Constants.sub)){
+				result = processSub(input.substring(4,input.length()-1),variables);
+			}else if(input.substring(0,3).equals(Constants.div)){
+				result = processDiv(input.substring(4,input.length()-1),variables);
+			}else if(input.substring(0,4).equals(Constants.mult)){
+				result = processMultiply(input.substring(5,input.length()-1),variables);
+			}else{
+				IllegalArgumentException e = new IllegalArgumentException("Syntax not valid");
+				log.error("Invalid operator Calculator.calculate "  + input);
+				throw e;
+			}
 		}else{
-			IllegalArgumentException e = new IllegalArgumentException("Operator not valid");
+			IllegalArgumentException e = new IllegalArgumentException("Syntax not valid");
 			log.error("Invalid operator Calculator.calculate "  + input);
 			throw e;
 		}
@@ -53,7 +72,15 @@ public class Calculator {
 		return result;
 	}
 
-	public static int processLet(String input,HashMap<String,Integer> variables) throws IllegalArgumentException{
+	/**
+	 * @param input
+	 * @param variables
+	 * @return
+	 * @throws IllegalArgumentException
+	 * Method to get result of a let operation
+	 */
+	public static int processLet(String input,HashMap<String,Integer> variables) 
+			throws IllegalArgumentException,NumberFormatException{
 		log.debug("ProcesLet called");
 		if(input.contains(",")){
 			int firstPos = getPos(input,0);
@@ -80,42 +107,72 @@ public class Calculator {
 				log.debug("ProcesLet completed");
 				return Integer.valueOf(varThree);
 			}
-
-
 		}else{
-			IllegalArgumentException e = new IllegalArgumentException("Operator not valid");
+			IllegalArgumentException e = new IllegalArgumentException("Syntax not valid");
 			log.error("Invalid operator Calculator.processLet " + input);
 			throw e;
 		}
-
-
 	}
 
 
-
-	public static int processAdd(String input,HashMap<String,Integer> variables) throws IllegalArgumentException{
+	/**
+	 * @param input
+	 * @param variables
+	 * @return
+	 * @throws IllegalArgumentException
+	 * Method to get result of a addition operation
+	 */
+	public static int processAdd(String input,HashMap<String,Integer> variables) 
+			throws IllegalArgumentException,RuntimeException,NumberFormatException{
 		log.debug("processAdd called");
-		int result =0;
+		long result =0;
 		int[] operands = extractOperands(input,variables);
 		result = operands[0] + operands[1];
+		if (result > Integer.MAX_VALUE) {
+			log.error("Integer max value exceeded in processAdd");
+			throw new RuntimeException("Overflow occured");
+		} else if (result < Integer.MIN_VALUE) {
+			log.error("Integer min value exceeded in processAdd");
+			throw new RuntimeException("Underflow occured");
+		}
 		log.debug("processAdd result" +result);
-		return result;
-
+		return (int)result;
 	}
 
-	public static int processSub(String input,HashMap<String,Integer> variables) throws IllegalArgumentException{
+	/**
+	 * @param input
+	 * @param variables
+	 * @return
+	 * @throws IllegalArgumentException
+	 * Method to get result of a subtraction operation
+	 */
+	public static int processSub(String input,HashMap<String,Integer> variables) 
+			throws IllegalArgumentException,RuntimeException,NumberFormatException{
 		log.debug("processSub called");
-		int result =0;
+		long result =0;
 		int[] operands = extractOperands(input,variables);
 		result = operands[0] - operands[1];
+		if (result > Integer.MAX_VALUE) {
+			log.error("Integer max value exceeded in processSub");
+			throw new RuntimeException("Overflow occured");
+		} else if (result < Integer.MIN_VALUE) {
+			log.error("Integer min value exceeded in processSub");
+			throw new RuntimeException("Underflow occured");
+		}
 		log.debug("processSub result" + result);
-		return result;
-
+		return (int)result;
 	}
 
-
+	/**
+	 * @param input
+	 * @param variables
+	 * @return
+	 * @throws IllegalArgumentException
+	 * @throws ArithmeticException
+	 * Method to get result of a division operation
+	 */
 	public static int processDiv(String input,HashMap<String,Integer> variables) 
-			throws IllegalArgumentException,ArithmeticException{	
+			throws IllegalArgumentException,ArithmeticException,NumberFormatException{	
 		log.debug("processDiv called");
 		int result =0;
 		try{	
@@ -123,16 +180,22 @@ public class Calculator {
 			result = operands[0] / operands[1];
 			log.debug("processDiv result" + result);
 			return result;
-			
+
 		}catch(ArithmeticException ar){
 			log.error("Arithmatic error for division of operands "+  input);
 			throw ar;
 		}
-		
-
 	}
 
-	public static int processMultiply(String input,HashMap<String,Integer> variables) throws IllegalArgumentException{
+	/**
+	 * @param input
+	 * @param variables
+	 * @return
+	 * @throws IllegalArgumentException
+	 * Method to get result of a multiply operation
+	 */
+	public static int processMultiply(String input,HashMap<String,Integer> variables) 
+			throws IllegalArgumentException,NumberFormatException{
 		log.debug("processMultiply called");
 		int result =0;
 		int[] operands = extractOperands(input,variables);
@@ -142,16 +205,25 @@ public class Calculator {
 
 	}
 
-	public static int[] extractOperands(String input,HashMap<String,Integer> variables){
+	/**
+	 * @param input
+	 * @param variables
+	 * @return
+	 * Method to calculate operand expression and return operand values.
+	 */
+	public static int[] extractOperands(String input,HashMap<String,Integer> variables) 
+			throws IllegalArgumentException,NumberFormatException{
 		log.debug("extractOperands called");
 		int[] result = new int[2];
 		if(input.contains(",")){
 			int seperatorPos = getPos(input,0);
 			String varOne = input.substring(0, seperatorPos);
 			int varOneVal = 0;
+			//check if expression has any operators
 			if(checkOperators(varOne)){
 				varOneVal = calculate(varOne,variables);
 			}else{
+				//check if Value is present in variables list
 				if(variables.containsKey(varOne)){
 					varOneVal= variables.get(varOne);
 				}else{
@@ -172,13 +244,19 @@ public class Calculator {
 			result[0]= varOneVal;
 			result[1] = varTwoVal;
 		}else{
-			IllegalArgumentException e = new IllegalArgumentException("Operator not valid");
+			IllegalArgumentException e = new IllegalArgumentException("Syntax not valid");
 			log.error("Invalid operator Calculator.extractOperands" + input);
 			throw e;
 		}
 		return result;
 	}
 
+
+	/**
+	 * @param inputStr
+	 * @return
+	 * Method to find if any operator is part of the expression
+	 */
 	public static boolean checkOperators(String inputStr )
 	{
 		String[] items = {Constants.add,Constants.let,Constants.mult,Constants.div,Constants.sub};
@@ -193,6 +271,17 @@ public class Calculator {
 		return false;
 	}
 
+
+	/**
+	 * @param input
+	 * @param num
+	 * @return
+	 * 
+	 * Method to find the position of ',' between two operands.
+	 * num is the operand number to be found
+	 * 
+	 * ex getPos("1,2" , 0) will give result 1 
+	 */
 	public static int getPos(String input,int num){
 		char[] arr = input.toCharArray();
 		int brackets =0;
@@ -217,6 +306,5 @@ public class Calculator {
 		}
 		log.debug("getPos completed");
 		return j;
-
 	}
 }
--- calc/src/test/java/calculator/calc/CalcTest.java
+++ calc/src/test/java/calculator/calc/CalcTest.java
@@ -10,36 +10,36 @@ public class CalcTest{
 
 	@Test
 	public void testAdd() {
-		assertEquals(Calculator.calculate("add(1,mult(2,3))",new HashMap<String,Integer>()), 7);
-		assertEquals(Calculator.calculate("add(1,2)",new HashMap<String,Integer>()), 3);
+		assertEquals(7,Calculator.calculate("add(1,mult(2,3))",new HashMap<String,Integer>()));
+		assertEquals(3,Calculator.calculate("add(1,2)",new HashMap<String,Integer>()));
 	}
 	
 	@Test
 	public void testLet() {
-		assertEquals(Calculator.calculate("let(a,5,add(a,a))",
-				new HashMap<String,Integer>()), 10);
-		assertEquals(Calculator.calculate("let(a,5,let(b,mult(a,10),add(b,a)))",
-				new HashMap<String,Integer>()), 55);
-		assertEquals(Calculator.calculate("let(a,let(b,10,add(b,b)),let(b,20,add(a,b)))",
-				new HashMap<String,Integer>()), 40);
+		assertEquals(10,Calculator.calculate("let(a,5,add(a,a))",
+				new HashMap<String,Integer>()));
+		assertEquals(55,Calculator.calculate("let(a,5,let(b,mult(a,10),add(b,a)))",
+				new HashMap<String,Integer>()));
+		assertEquals(40,Calculator.calculate("let(a,let(b,10,add(b,b)),let(b,20,add(a,b)))",
+				new HashMap<String,Integer>()));
 		
 	}
 	
 	@Test
 	public void testMult() {
-		assertEquals(Calculator.calculate("mult(add(2,2),div(9,3))",
-				new HashMap<String,Integer>()), 12);
+		assertEquals(12,Calculator.calculate("mult(add(2,2),div(9,3))",
+				new HashMap<String,Integer>()));
 	}
 	
 	
 	@Test
 	public void testSub(){
-		assertEquals(Calculator.calculate("sub(2,1)",new HashMap<String,Integer>()), 1);
+		assertEquals(1,Calculator.calculate("sub(2,1)",new HashMap<String,Integer>()));
 	}
 	
 	@Test
 	public void testDiv(){
-		assertEquals(Calculator.calculate("div(2,1)",new HashMap<String,Integer>()), 2);
+		assertEquals(2,Calculator.calculate("div(2,1)",new HashMap<String,Integer>()));
 	}
 	
 	@Test(expected=java.lang.ArithmeticException.class)
